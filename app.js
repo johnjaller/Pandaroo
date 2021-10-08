2869a090b993a6b500d734435280b32ddc91de54
Sherman <shermanxcc@gmail.com>
2021-10-09 01:22:28 +0800

    Added user login page layout

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
 //import package
-const express=require('express')
-const handlebars=require("express-handlebars")
-const cors=require('cors')
-const app=express()
-const http=require('http').Server(app)
-const io=require('socket.io')(http)
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
 //middleware
-app.use(cors())
-app.use(express.static("public"))
-app.engine("handlebars",handlebars({defaultLayout:"main"}))
-app.use(express.urlencoded({extended:false}))
-app.set("view engine","handlebars")
-app.use(express.json())
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Set up handlebars
+const handlebars = require("express-handlebars");
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 //route
-app.get('/',(req,res)=>{
-    res.send('Hello World')
-})
+// app.get("/", (req, res) => {
+//   res.render("Hello World");
+// });
 
+app.get("/login", (req, res) => {
+  res.render("user-login-signup");
+});
 
-http.listen(8080,()=>{
-    console.log('app listeninf to port 8080')
-})
+http.listen(8080, () => {
+  console.log("app listening to port 8080");
+});
 
-module.exports={http,app}
+module.exports = { http, app };
