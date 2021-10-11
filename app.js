649ee0c2505b6d7fed3ff10365e26d6c685310f5
Hayley Chu <hayley109109@gmail.com>
2021-10-11 10:38:12 +0800

    Updated restaurant header

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Set up handlebars
-const handlebars = require("express-handlebars")
+const handlebars = require("express-handlebars");
 app.engine("handlebars", handlebars({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
@@ -60,6 +60,7 @@ app.get("/bookingshistory", (req, res) => {
 
 app.get("/ordershistory", (req, res) => {
   res.render("restOrderHistory", { layout: "restaurant" });
+});
 
 // Sher: Temporary route set up for testing sign in page
 app.get("/login", (req, res) => {
@@ -74,4 +75,4 @@ http.listen(8080, () => {
   console.log("app listening to port 8080");
 });
 
-module.exports = { http, app };
+module.exports = { app, http };
